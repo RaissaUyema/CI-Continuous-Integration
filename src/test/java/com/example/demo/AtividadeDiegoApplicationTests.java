312e58e55dfb7eed1eb8075375c60b6aeebc5922
RaissaUyema <raissa.meggiolaro@sptech.school>
2025-05-26 22:11:41 -0300

    Atividade

--- src/test/java/com/example/demo/AtividadeDiegoApplicationTests.java
+++ src/test/java/com/example/demo/AtividadeDiegoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 class AtividadeDiegoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Controller service = new Controller();
+		String resultado = service.hello();
+		assertEquals("Teste", resultado);
 	}
 
 }
