250becd9a2e9eb0b2bbeafee37419f7ad090236d
RaissaUyema <raissa.meggiolaro@sptech.school>
2025-05-26 22:05:07 -0300

    Atividade

---
+++ src/main/java/com/example/demo/AtividadeDiegoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeDiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeDiegoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+        @GetMapping("/atividade")
+        public String hello() { return "olá mundo cruel";}
+
+}
